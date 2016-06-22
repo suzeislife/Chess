c4a58dd2f8e2bd76780ff5d7502b80d3635f9893
Jeremy Portelli <jeremyp.1994@hotmail.com>
2016-06-22 19:34:28 +0200

    V1.5

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,23 @@
 package application;
 
+import javax.swing.ImageIcon;
+
+import Piece.Cavalier;
+import Piece.Fou;
+import Piece.Pion;
+import Piece.Reine;
+import Piece.Roi;
+import Piece.Tour;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -42,7 +54,7 @@ public class Main extends Application {
 		// Set element top
 
 		// Set top element
-		top.setPadding(new Insets(15,15,15,15));
+		top.setPadding(new Insets(15, 15, 15, 15));
 		top.getChildren().add(buttonStart);
 		top.getChildren().add(buttonReset);
 		top.getChildren().add(player);
@@ -52,26 +64,30 @@ public class Main extends Application {
 		createGrid();
 		white.setStyle("-fx-background-color : white");
 		black.setStyle("-fx-background-color : black");
-		
-		//set PanelBlanc/Noir
+
+		// set PanelBlanc/Noir
 		panelBlanc.getChildren().add(white);
 		panelNoir.getChildren().add(black);
-		
+
 		// Set mid element
-		mid.setPadding(new Insets(15,15,15,15));
+		mid.setPadding(new Insets(15, 15, 15, 15));
 		mid.getChildren().add(chessGrid);
 		mid.getChildren().add(panelBlanc);
 		mid.getChildren().add(panelNoir);
 
 		root.getChildren().add(top);
 		root.getChildren().add(mid);
+		// Event button
+		buttonStart.setOnAction(event -> {
+			initNewGame();
+		});
 	}
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			primaryStage.setTitle("ChessGame");
-			Scene scene = new Scene(root,700,600);
+			Scene scene = new Scene(root, 700, 600);
 
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -88,24 +104,96 @@ public class Main extends Application {
 		chessGrid.setPrefSize(600, 600);
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
-				initGrid = new Label();
+				tab[i][j] = new Label();
+				tab[i][j].setOnMouseClicked(new EventHandler<Event>() {
+
+					@Override
+					public void handle(Event event) {
+						for (int i = 0; i < 8; i++) {
+							for (int j = 0; j < 8; j++) {
+								if(event.getSource().equals(tab[i][j])){
+									changePion(i, j);
+								}
+							}
+						}
+					}
+					
+				});
+
+				
 				if (i % 2 == 0 && j % 2 == 0) {
-					initGrid.setStyle("-fx-background-color : black");
+					tab[i][j].setStyle("-fx-background-color : black");
 				} else if (j % 2 != 0 && i % 2 == 0) {
-					initGrid.setStyle("-fx-background-color : white");
+					tab[i][j].setStyle("-fx-background-color : white");
 				} else if (j % 2 == 0 && i % 2 != 0) {
-					initGrid.setStyle("-fx-background-color : white");
+					tab[i][j].setStyle("-fx-background-color : white");
 				} else {
-					initGrid.setStyle("-fx-background-color : black");
+					tab[i][j].setStyle("-fx-background-color : black");
 				}
-				initGrid.setPrefSize(60, 60);
-				chessGrid.add(initGrid, i, j);
+				tab[i][j].setPrefSize(60, 60);
+				chessGrid.add(tab[i][j], i, j);
 			}
 
 		}
 	}
 
 	public void initNewGame() {
+		/*
+		 * Piece pieceTampon = null; ImageView iconeTampon; int ligneClic; int
+		 * colonneClic; Position depart, arrivee; String couleurControle =
+		 * "blanc"; Position temp = null;
+		 */
+
+		// Init J1
+		currentPlayer.setText("J1");
+		echiquier.debuter();
+		String dossierIcone = "/src/Icone/";
+		char[] ordrePiece = { 'T', 'C', 'F', 'D', 'R', 'F', 'C', 'T' };
+		int increment = 1;
+		int ligne = 0;
+		char couleur = 'N';
+		Piece tempo = null;
+		echiquier.debuter();
+
+		while (increment >= -1) {
+			for (int i = 0; i < 8; i++) {
+				System.out.println(dossierIcone + ordrePiece[i] + couleur + ".gif");
+				tab[i][ligne].setGraphic(new ImageView(dossierIcone + ordrePiece[i] + couleur + ".gif"));
+				switch (ordrePiece[i]) {
+				case 'T':
+					tempo = new Tour(ligne < 5 ? "noir" : "blanc");
+					break;
+
+				case 'C':
+					tempo = new Cavalier(ligne < 5 ? "noir" : "blanc");
+					break;
+
+				case 'F':
+					tempo = new Fou(ligne < 5 ? "noir" : "blanc");
+					break;
+
+				case 'D':
+					tempo = new Reine(ligne < 5 ? "noir" : "blanc");
+					break;
+
+				case 'R':
+					tempo = new Roi(ligne < 5 ? "noir" : "blanc");
+					break;
+
+				}
+				echiquier.getCase(i, ligne).setPiece(tempo);
+				tab[i][ligne + increment].setGraphic(new ImageView(dossierIcone + 'P' + couleur + ".gif"));
+				echiquier.getCase(i, ligne + increment).setPiece(new Pion(ligne < 5 ? "noir" : "blanc"));
+
+			}
+			couleur = 'B';
+			increment -= 2;
+			ligne = 7;
+		}
+
+	}
+
+	public void changePion(int ligne, int colonne) {
 		
 	}
 
